f953bb4aac7bdcc75acaa36b172b4bc0671811c4
Rémi Bastide <Remi.Bastide@irit.fr>
2023-12-15 16:17:15 +0100

    Préparation TP

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,11 @@
 -- Initialisation des tables
 INSERT INTO Country(id, code, name) VALUES
-    (1, 'FR', 'France'), -- Les clés sont auto-générées
-    (2, 'UK', 'United Kingdom'),
-    (3, 'US', 'United States of America');
--- On peut fixer la valeur des clés auto-générées, mais il faut ensuite
+-- Les clés sont auto-générées, donc on ne les spécifie pas
+    (default, 'FR', 'France'), -- id = 1
+    (default, 'UK', 'United Kingdom'), -- id = 2
+    (default, 'US', 'United States of America'); -- id = 3
+-- On peut fixer la valeur des clés auto-générées, mais il faudrait ensuite
 -- réinitialiser le compteur de clé auto-générée
 -- Attention : la syntaxe est différente selon le SGBD utilisé
-ALTER TABLE Country ALTER COLUMN id RESTART WITH 4;
+-- ALTER TABLE Country ALTER COLUMN id RESTART WITH 4;
 
