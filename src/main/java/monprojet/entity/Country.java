48e0d32601a9fb9cf8b46809857affd4cff80695
sbenyahm <sinda.ben_yahmed@etud.univ-jfc.fr>
2024-01-08 11:28:19 +0100

    etape1

--- src/main/java/monprojet/entity/Country.java
+++ src/main/java/monprojet/entity/Country.java
@@ -23,4 +23,9 @@ public class Country {
     @Column(unique=true)
     @NonNull
     private String name;
+    // Dans la classe "Country.java"
+    @OneToMany(mappedBy="country")
+    // Essayer sans "mappedBy" pour voir le schémma relationnel généré
+    // @OneToMany
+    private List<City> cities = new ArrayList<>();
 }
